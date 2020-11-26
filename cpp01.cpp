48be6b50cd79a902a8840bafada8fa8a44b3e76f
YoungHa Kim <ytok1108@aol.com>
2020-11-26 22:39:50 +0900

    Hello cpp

---
+++ cpp01.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "std_lib_facilities.h"
+using namespace std;
+int main()
+{
+	cout << "성과 이름을 입력하세요\n ";
+	string first;
+	string second;
+	cin >> first >> second;
+	string name = first + ' ' + second;
+	cout << "안녕하세요, " << name << '\n';
+}
