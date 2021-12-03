735f627dedd07cb7647bf4db3aa6573ad4452249
YoungHa Kim <ytok1108@aol.com>
2021-12-03 22:17:20 +0900

    vec2

--- cpp01.cpp
+++ cpp01.cpp
@@ -3,15 +3,10 @@
 
 int main()
 {
-	std::vector<int> array;
-
-	std::vector<int> array2 = {1, 2, 3, 4, 5};
-
-	std::cout << array2.size() << std::endl;
-
-	std::vector<int> array3 = {1, 2, 3};
-
-	std::cout << array3.size() << std::endl;
+	std::vector<int> arr = {1, 2, 3, 4, 5};
+	for (auto &itr : arr)
+		std::cout << itr << " ";
+	std::cout << std::endl;
 
 	return 0;
 }
