feb760c644df3ec7074139d36364985c3ecd5dd4
YoungHa Kim <ytok1108@aol.com>
2021-12-04 14:37:56 +0900

    vector erase

--- cpp01.cpp
+++ cpp01.cpp
@@ -22,7 +22,16 @@ int main()
 
 	myVector.insert(myVector.begin() + 3, 5);
 
-	cout << "Vector: ";
+	cout << "\nVector: ";
+
+	for (unsigned int i = 0; i < myVector.size(); i++)
+	{
+		cout << myVector[i] << " ";
+	}
+
+	myVector.erase(myVector.begin() + 4);
+
+	cout << "\nVector: ";
 
 	for (unsigned int i = 0; i < myVector.size(); i++)
 	{
