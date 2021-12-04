57490f5962b19a4e57c47545d217d19e54c00fc0
YoungHa Kim <ytok1108@aol.com>
2021-12-04 14:58:16 +0900

    vector as parameters 1

--- cpp01.cpp
+++ cpp01.cpp
@@ -3,66 +3,41 @@
 
 using namespace std;
 
+void fillVector(vector<int> &);
+
+void printVector(const vector<int> &);
+
 int main()
 {
 	vector<int> myVector;
 
-	myVector.push_back(3);
-	myVector.push_back(7);
-	myVector.push_back(4);
-	myVector.push_back(12);
-	myVector.push_back(9);
+	fillVector(myVector);
+	printVector(myVector);
+
+	return 0;
+}
+
+void fillVector(vector<int> &newMyVector)
+{
+	cout << "TYpe in list of numbers ( -1 top stop): ";
+	int input;
+	cin >> input;
+
+	while (input != -1)
+	{
+		newMyVector.push_back(input);
+		cin >> input;
+	}
+	cout << endl;
+}
+
+void printVector(const vector<int> &newMyVector)
+{
 
 	cout << "Vector: ";
 
-	for (unsigned int i = 0; i < myVector.size(); i++)
+	for (unsigned int i = 0; i < newMyVector.size(); i++)
 	{
-		cout << myVector[i] << " ";
+		cout << newMyVector[i] << " ";
 	}
-
-	myVector.insert(myVector.begin() + 3, 5);
-
-	cout << "\nVector: ";
-
-	for (unsigned int i = 0; i < myVector.size(); i++)
-	{
-		cout << myVector[i] << " ";
-	}
-
-	myVector.erase(myVector.begin() + 4);
-
-	cout << "\nVector: ";
-
-	for (unsigned int i = 0; i < myVector.size(); i++)
-	{
-		cout << myVector[i] << " ";
-	}
-
-	if (myVector.empty())
-	{
-		cout << endl
-			 << "Is Empty!";
-	}
-	else
-	{
-		cout << endl
-			 << "Is Not Empty";
-	}
-
-	myVector.clear();
-
-	if (myVector.empty())
-	{
-		cout << endl
-			 << "Is Empty!";
-	}
-	else
-	{
-		cout << endl
-			 << "Is Not Empty";
-	}
-
-	cout << endl;
-
-	return 0;
 }
