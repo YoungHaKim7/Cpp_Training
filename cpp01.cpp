584ba3ff2c819c625b9ba01d144491cb62ec2c31
YoungHa Kim <ytok1108@aol.com>
2021-12-06 22:01:01 +0900

    cpp basic int two cin

--- cpp01.cpp
+++ cpp01.cpp
@@ -1,43 +1,21 @@
-﻿#include <vector>
-#include <iostream>
+﻿#include <iostream>
 
 using namespace std;
 
-void fillVector(vector<int> &);
-
-void printVector(const vector<int> &);
-
 int main()
 {
-	vector<int> myVector;
+	int a;
+	int b;
+	int sum;
 
-	fillVector(myVector);
-	printVector(myVector);
+	cout << "Enter a number hoss! \n";
+	cin >> a;
+
+	cout << "Enter another number \n";
+	cin >> b;
+
+	sum = a + b;
+	cout << "the sum of those numbers is " << sum << endl;
 
 	return 0;
-}
-
-void fillVector(vector<int> &newMyVector)
-{
-	cout << "TYpe in list of numbers ( -1 top stop): ";
-	int input;
-	cin >> input;
-
-	while (input != -1)
-	{
-		newMyVector.push_back(input);
-		cin >> input;
-	}
-	cout << endl;
-}
-
-void printVector(const vector<int> &newMyVector)
-{
-
-	cout << "Vector: ";
-
-	for (unsigned int i = 0; i < newMyVector.size(); i++)
-	{
-		cout << newMyVector[i] << " ";
-	}
 }
