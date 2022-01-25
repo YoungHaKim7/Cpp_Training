4fbcea48f190026f699e41dbf7d7ec916fc9b5cc
YoungHa Kim <ytok1108@aol.com>
2022-01-26 00:18:32 +0900

    vec4 print

--- cpp01.cpp
+++ cpp01.cpp
@@ -10,7 +10,7 @@ void display(vector<T> &v)
 	for (int i = 0; i < v.size(); i++)
 	{
 		cout << v[i] << " ";
-		cout << v.at(i) << " ";
+		// cout << v.at(i) << " ";
 	}
 	cout << endl;
 }
@@ -18,12 +18,14 @@ void display(vector<T> &v)
 int main()
 {
 	// Ways to create a vector
-	vector<int> vec1;		 // zero length integer vector
-	vector<char> vec2(4);	 // 4-element character vector
-	vector<char> vec3(vec2); // 4-element character vector from vec2
-	vector<int> v(6, 3);	 // 6-element vector of 3s
-	vec2.push_back('5');
-	display(vec2);
+	vector<int> vec1;	  // zero length integer vector
+	vector<char> vec2(4); // 4-element character vector
+	// vec2.push_back('5');
+	// display(vec2);
+	// vector<char> vec3(vec2); // 4-element character vector from vec2
+	// display(vec3);
+	vector<int> vec4(6, 3); // 6-element vector of 3s
+	display(vec4);
 
 	int element, size = 5;
 	// cout << "Enter the size of your vector" << endl;
