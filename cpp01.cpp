e272cde4bb6172d341c0f5d86d78555cebdc9a82
YoungHa Kim <ytok1108@aol.com>
2022-01-26 00:15:07 +0900

    vec___

--- cpp01.cpp
+++ cpp01.cpp
@@ -2,9 +2,11 @@
 #include <vector>
 
 using namespace std;
+template <class T>
 
-void display(vector<int> &v)
+void display(vector<T> &v)
 {
+	cout << "Displaying this vector " << endl;
 	for (int i = 0; i < v.size(); i++)
 	{
 		cout << v[i] << " ";
@@ -15,21 +17,29 @@ void display(vector<int> &v)
 
 int main()
 {
-	vector<int> vec1;
-	int element, size;
-	cout << "Enter the size of your vector" << endl;
-	cin >> size;
-	for (int i = 0; i < size; i++)
-	{
-		cout << "Enter an element to add to this vector : ";
-		cin >> element;
-		vec1.push_back(element);
-	}
+	// Ways to create a vector
+	vector<int> vec1;		 // zero length integer vector
+	vector<char> vec2(4);	 // 4-element character vector
+	vector<char> vec3(vec2); // 4-element character vector from vec2
+	vector<int> v(6, 3);	 // 6-element vector of 3s
+	vec2.push_back('5');
+	display(vec2);
+
+	int element, size = 5;
+	// cout << "Enter the size of your vector" << endl;
+	// cin >> size;
+	// for (int i = 0; i < size; i++)
+	// {
+	// 	cout << "Enter an element to add to this vector : ";
+	// 	cin >> element;
+	// 	vec1.push_back(element);
+	// }
 	// vec1.pop_back();
-	display(vec1);
-	vector<int>::iterator iter = vec1.begin();
-	vec1.insert(iter + 1, 5, 566);
-	display(vec1);
+
+	// display(vec1);
+	// vector<int>::iterator iter = vec1.begin();
+	// // vec1.insert(iter + 1, 5, 566);
+	// display(vec1);
 
 	return 0;
 }
