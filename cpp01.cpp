f557647468c39327f648771696376ff9c37dbca3
YoungHa Kim <ytok1108@aol.com>
2022-01-25 23:30:44 +0900

    Vector 001

--- cpp01.cpp
+++ cpp01.cpp
@@ -1,38 +1,18 @@
 ﻿#include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int main()
 {
-	vector<int> a;
-	int n, x;
-	cout << "Please enter a number to convert. : ";
-	cin >> n;
-	cout << endl;
-
-	for (int i = 0; i < n; i++)
+	vector<int> vec1;
+	int element;
+	for (int i = 0; i < 4; i++)
 	{
-		cin >> x;
-		a.push_back(x);
+		cout << "Enter an element to add to this vector";
+		cin >> element;
+		vec1.push_back(element);
 	}
-
-	for (int i = 0; i < n; i++)
-		cout << a[i] << " ";
-	cout << endl;
-
-	sort(a.begin(), a.end());
-
-	for (int i = 0; i < n; i++)
-		cout << a[i] << " ";
-	cout << endl;
-
-	sort(a.begin(), a.end(), greater<int>());
-
-	for (int i = 0; i < n; i++)
-		cout << a[i] << " ";
-	cout << endl;
 
 	return 0;
 }
