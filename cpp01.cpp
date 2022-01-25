621e802aecf0f3d177b7a2d38c029d392b447adf
YoungHa Kim <ytok1108@aol.com>
2022-01-25 23:34:08 +0900

    Vec print

--- cpp01.cpp
+++ cpp01.cpp
@@ -3,6 +3,15 @@
 
 using namespace std;
 
+void display(vector<int> &v)
+{
+	for (int i = 0; i < v.size(); i++)
+	{
+		cout << v[i] << " ";
+	}
+	cout << endl;
+}
+
 int main()
 {
 	vector<int> vec1;
@@ -13,6 +22,7 @@ int main()
 		cin >> element;
 		vec1.push_back(element);
 	}
+	display(vec1);
 
 	return 0;
 }
