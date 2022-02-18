2883092c5654c9cc2a7266d2857b0d005af696cc
YoungHa Kim <ytok1108@aol.com>
2022-02-19 00:40:37 +0900

    sum result: First number : 13 Second nubmer : 7  Sum :  20

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
+
 int main(int argc, char **argv) {
-    int firstNumber = 12;
-    int secondNumber = 9;
 
-    int sum = firstNumber + secondNumber;
+    int first_number {13}; // Statement
+    int second_number {7};
 
-    std::cout << "The sum of the two numbers is : " << sum << std::endl;
+    std::cout << "First number : " << first_number << std::endl;
+    std::cout << "Second nubmer : " << second_number << std::endl;
+
+    int sum = first_number + second_number;
+    std::cout << " Sum :  " << sum << std::endl;
+
+    
 
     return 0;
 }
