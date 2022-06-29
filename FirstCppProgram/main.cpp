2c88748134fbbb63ffcbd0b1bad00c1f4217d1d2
YoungHa Kim <ytok1108@aol.com>
2022-06-29 20:24:04 +0900

    hello world___cpp__thread

--- FirstCppProgram/main.cpp
+++ FirstCppProgram/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+int addNumbers( int first_param, int second_param) {
+
+    int result = first_param + second_param;
+    return result;
+    
+}
 
 int main(int argc, char **argv) {
 
@@ -11,8 +17,10 @@ int main(int argc, char **argv) {
 
     int sum = first_number + second_number;
     std::cout << " Sum :  " << sum << std::endl;
+    std::cout << " Sum :  " << sum << std::endl;
 
     
+    sum = addNumbers(25, 7);
 
     return 0;
 }
---
+++ simplecpp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <thread>
+using namespace std;
+
+void hello() { cout << "Hello I am thread \n"; }
+
+int main(int argc, char *argv[]) {
+  thread t(hello);
+  t.join();
+  return 0;
+}
